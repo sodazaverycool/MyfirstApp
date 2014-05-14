c8b9489c99f034e378389f0cca8904c9fe0fa44f
soulda <>
2014-05-14 09:09:59 +0700

    สร้างโปรเจคครั้งเเรก

---
+++ src/myfirstapp/MyfirstApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myfirstapp;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MyfirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Patipan Vongsom");
+        System.out.println("55021339");
+    }
+    
+}
